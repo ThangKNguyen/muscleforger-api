148d9a00aa4853e3d2e69e529b920ed6da852392
Thang Nguyen <thangknguyensj@gmail.com>
2026-03-15 00:24:51 -0700

    Initial commit

---
+++ src/main/java/com/muscleforger/api/MuscleforgerApiApplication.java
@@ -0,0 +1,15 @@
+package com.muscleforger.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuscleforgerApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MuscleforgerApiApplication.class, args);
+		qweqwe
+	}
+
+}
---
+++ src/test/java/com/muscleforger/api/MuscleforgerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muscleforger.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuscleforgerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
